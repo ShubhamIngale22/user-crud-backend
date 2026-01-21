a17060e1b29ab0100f05ffaeb3248a2a3cccf0a5
pratikj <shubhingale23052004@gmail.com>
2026-01-21 11:38:16 +0530

    upload middleware

--- app/middlewares/upload.middleware.js
+++ app/middlewares/upload.middleware.js
@@ -4,14 +4,14 @@ const dateHelper=require('../helpers/date.helper')
 const path = require('path');
 
 const storage=multer.diskStorage({
-        destination:(req,file,cb)=> {
-            cb(null, 'uploads/');
-        },
-        filename: (req,file,cb)=> {
-            const timeStamp=dateHelper.nowIST().format('DD-MM-YYYY_hh-mm-ss');
-            cb(null,`${timeStamp}${path.extname(file.originalname)}`);
-            // cb(null,Date.now()+path.extname(file.originalname));
-        }
+    destination:(req,file,cb)=> {
+        cb(null, 'uploads/');
+    },
+    filename: (req,file,cb)=> {
+        const timeStamp=dateHelper.nowIST().format('DD-MM-YYYY_hh-mm-ss');
+        cb(null,`${timeStamp}${path.extname(file.originalname)}`);
+        // cb(null,Date.now()+path.extname(file.originalname));
+    }
 });
 
 const fileFilter=(req,file,cb)=>{
