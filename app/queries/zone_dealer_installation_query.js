5cf20ea4a0a525f9818b0eca28d71f638d00887d
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-04 18:01:45 +0530

    Feat: Creating function of zone-wise installation query.

---
+++ app/queries/zone_dealer_installation_query.js
@@ -0,0 +1,31 @@
+const DealerInstallations=require("../models/DealerInstallations");
+
+const zoneDealerInstallationsQuery=(startDate)=>{
+    return DealerInstallations.aggregate([
+        {
+            $match:{
+                installationDate:{$gte:startDate}
+            }
+        },
+        {
+            $group:{
+                _id:"$zone",
+                count:{$sum:1}
+            }
+        },
+        {
+            $project:{
+                _id:0,
+                zone:"$_id",
+                count:1
+            }
+        },
+        {
+            $sort:{
+                count:-1
+            }
+        }
+    ])
+};
+
+module.exports=zoneDealerInstallationsQuery;
