dd1280388b56bdc7e20dfce1a42651dcf76714d0
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-04 16:11:54 +0530

    Feat: Creating service for the dealer_installations.

--- app/services/dealer_installations.js
+++ app/services/dealer_installations.js
@@ -1,45 +1,8 @@
-const DealerInstallations=require("../models/DealerInstallations");
+const dealerInstallationsQuery=require("../queries/DealerInstallation_query");
 
 module.exports= {
-    totalInstallations: () => {
-
-    let q = DealerInstallations.aggregate([
-            {
-                $facet: {
-                    last24Hours: [
-                        {
-                            $match: {
-                                installationDate: {
-                                    $gte: new Date(Date.now() - 24 * 60 * 60 * 1000)
-                                }
-                            }
-                        },
-                        {$count: "count"}
-                    ],
-                    last1Month: [
-                        {
-                            $match: {
-                                installationDate: {
-                                    $gte: new Date(new Date().setMonth(new Date().getMonth() - 1))
-                                }
-                            }
-                        },
-                        {$count: "count"}
-                    ],
-                    last1Year: [
-                        {
-                            $match: {
-                                installationDate: {
-                                    $gte: new Date(new Date().setFullYear(new Date().getFullYear() - 1))
-                                }
-                            }
-                        },
-                        {$count: "count"}
-                    ]
-                }
-            }
-        ]);
-        return q.then(result => {
+    totalDealerInstallations: () => {
+        return dealerInstallationsQuery().then(result => {
             const r = result[0] || {};
             return {
                 last24Hours: r.last24Hours?.[0]?.count || 0,
@@ -47,12 +10,7 @@ module.exports= {
                 last1Year: r.last1Year?.[0]?.count || 0
             };
         });
-    },
-
-    zoneInstallations:()=>{
     }
-
-
 }
 
 
