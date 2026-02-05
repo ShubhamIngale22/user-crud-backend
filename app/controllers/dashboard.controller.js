f0c84a6db5d24a6219be4178798de81beda44a69
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-05 12:59:58 +0530

    Creating one controller for line-chart dealer installations.

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -1,5 +1,6 @@
 const response = require("../utilities/api_handler");
 const dealerInstallService=require("../services/dealer_installations");
+const chartFormatter = require("../utilities/chartFormatter");
 
 module.exports = {
     dealerInstallationTable: (req, res) => {
@@ -20,5 +21,19 @@ module.exports = {
             console.error(err);
             return res.json(response.JsonMsg(false, null , "Failed to fetch data", 500));
         })
+    },
+
+    DealerInstallationsLineChart: (req, res) => {
+        const type=req.query.type || "7days";
+
+        return dealerInstallService.DealerInstallationsLine(type).then((data)=>{
+
+            const LineChartData=chartFormatter.lineChart(data);
+
+            return res.json(response.JsonMsg(true,LineChartData, "Dealer Installations Data for Line-Chart", 200));
+        }).catch((err)=>{
+            console.error(err);
+            return res.json(response.JsonMsg(false, null , "Failed to fetch data", 500));
+        })
     }
-};
+}
