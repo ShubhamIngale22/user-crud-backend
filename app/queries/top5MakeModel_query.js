f6f5d5a62f1e6e22d45c6b66dddc2302b3f2112f
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-05 18:01:08 +0530

    Feat: Creating new query function to count make-model installations.

---
+++ app/queries/top5MakeModel_query.js
@@ -0,0 +1,36 @@
+const DealerInstallations = require("../models/DealerInstallations");
+
+const top5MakeModelQuery= ()=>{
+    return DealerInstallations.aggregate([
+        {
+            $match: {
+                manufacturerName: { $ne: null },
+                vehicleModelNo: { $ne: null }
+            }
+        },
+        {
+            $group: {
+                _id:{
+                    make:"$manufacturerName",
+                    model:"$vehicleModelNo"
+                } ,
+                count: { $sum: 1 }
+            },
+        },
+        {
+            $sort: {count:-1}
+        },
+        {
+            $limit:5
+        },
+        {
+            $project:{
+                _id:0,
+                Make:"$_id.make",
+                Model:"$_id.model",
+                count:1
+            }
+        }
+    ])
+}
+module.exports= top5MakeModelQuery;
