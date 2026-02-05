a35c1020b3b2bd9052bf9e1d71fe5f80fd2e5853
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-05 12:58:44 +0530

    Feat: Creating one service for line-chart dealer installations.

--- app/services/dealer_installations.js
+++ app/services/dealer_installations.js
@@ -1,5 +1,6 @@
 const dealerInstallationsQuery=require("../queries/DealerInstallation_query");
 const zoneDealerInstallationsQuery=require("../queries/zone_dealer_installation_query");
+const dealerInstallationsLineQuery=require("../queries/dealerInstallation_line_query");
 const moment=require("moment");
 
 module.exports= {
@@ -25,8 +26,26 @@ module.exports= {
             startDate = moment().subtract(1, "month").startOf("month").toDate();
             endDate   = moment().subtract(1, "month").endOf("month").toDate();
         }
-
         return zoneDealerInstallationsQuery(startDate, endDate)
+            .then(result => result);
+    },
+
+    DealerInstallationsLine: (type) => {
+
+        let startDate;
+        let format;
+
+        if (type === "1year") {
+            startDate = moment().subtract(1, "year").toDate();
+            format= "%Y-%m";
+        } else if (type === "30days") {
+            startDate = moment().subtract(30, "days").toDate();
+            format= "%Y-%m-%d";
+        }else{
+            startDate = moment().subtract(7, "day").toDate();
+            format= "%Y-%m-%d";
+        }
+        return dealerInstallationsLineQuery(startDate,format)
             .then(result => result);
     }
 }
