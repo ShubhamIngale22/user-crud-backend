934f98796289094e38497424833ae27d522bdf81
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-09 13:02:20 +0530

    Feat: Creating one query function for calculating top 5 zones in installations.

---
+++ app/queries/top5zones_query.js
@@ -0,0 +1,33 @@
+const DealerInstallations = require("../models/DealerInstallations");
+
+const top5zonesQuery = () => {
+    return DealerInstallations.aggregate([
+        {
+            $match: {
+                zone: { $ne: null }
+            }
+        },
+        {
+            $group: {
+                _id: "$zone",
+                count: { $sum: 1 }
+            },
+        },
+        {
+            $sort: {count:-1}
+        },
+        {
+            $limit:5
+        },
+        {
+            $project: {
+                _id: 0,
+                zone: "$_id",
+                count: 1
+            }
+        }
+
+    ])
+}
+
+module.exports = top5zonesQuery;
